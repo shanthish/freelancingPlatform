3b1f437370f8ab4a77e62c16ae77dd31c8182494
shanthish23 <shanthish.sr2023cse@sece.ac.in>
2025-01-31 09:50:28 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -67,8 +67,8 @@ app.post('/add-gig',async (req,res)=>{
             revision,
             features,
         });
-        // let user_data=await User.findById('679be1faf732dfa38448cbb8');
-        // new_gig.seller_id.push(user_data._id);
+        let user_data=await User.findById('679be1faf732dfa38448cbb8');
+        new_gig.seller_id.push(user_data._id);
         await new_gig.save();
         res.status(200).json({data:user_data,message:true});
     }
